f9528f1bd0a8793ba73b1a64f384043249bb0e5e
Ivan-Najdovski <ivan.najdovski@keitarto.com>
2019-04-30 16:34:24 +0200

    updated gulpfile

--- ckanext/amplus/fanstatic/gulpfile.js
+++ ckanext/amplus/fanstatic/gulpfile.js
@@ -24,11 +24,7 @@ gulp.task('less', function () {
 
 gulp.task('fonts', function() {
   return gulp.src('node_modules/typeface-barlow/files/*')
-<<<<<<< HEAD
-    .pipe(gulp.dest('fonts'))
-=======
     .pipe(gulp.dest('public/fonts'))
->>>>>>> master
 })
 
 // Watch for LESS file changes
