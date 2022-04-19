5f5aa9de1c82e0af5ce503640f44ad575750c4bf
Jovana <jovana.klimovska@keitaro.com>
2022-04-19 13:20:38 +0200

    Use sas instead of node-sass

--- ckanext/amplus/assets/gulpfile.js
+++ ckanext/amplus/assets/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+var sass = require('sass')(require('sass'));
 var autoprefixer = require('gulp-autoprefixer');
 // var sourcemaps = require('gulp-sourcemaps'); - Uncomment when developing
 
