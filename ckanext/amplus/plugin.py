f093736c635d5d72a05a06fa2ca0786e3ddfb4d5
Petar Popovski <petar.popovski@keitaro.com>
2024-04-03 13:19:29 +0200

    Style changes and bug fixes for ckan 2.10

--- ckanext/amplus/plugin.py
+++ ckanext/amplus/plugin.py
@@ -9,7 +9,12 @@ class AmplusPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
+        ckan_version = helpers.get_ckan_version()
+        if (ckan_version.startswith('2.9')):
+            toolkit.add_template_directory(config_, 'templates')
+        else:
+            toolkit.add_template_directory(config_, 'templates-2-10')
+
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'amplus')
 
@@ -18,12 +23,14 @@ class AmplusPlugin(plugins.SingletonPlugin):
             'amplus_get_recently_updated_datasets':
                 helpers.get_recently_updated_datasets,
             'amplus_get_site_statistics':
-                helpers.get_site_statistics
+                helpers.get_site_statistics,
+            'get_ckan_version':
+                helpers.get_ckan_version
         }
 
     def update_config_schema(self, schema):
         ignore_missing = toolkit.get_validator('ignore_missing')
-        validators = [ignore_missing, str]
+        validators = [ignore_missing, ignore_missing]
 
         schema.update({
             'homepage_blogs': validators,
